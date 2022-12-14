ae7d0696fb16551556c461aaecd63b3ed33554ac
electricessence <5899455+electricessence@users.noreply.github.com>
2022-12-13 17:02:52 -0800

    Cleanup.

--- MortgageInfo.Console/Extensions.cs
+++ MortgageInfo.Console/Extensions.cs
@@ -9,26 +9,6 @@ using Spectre.Console;
 namespace MortgageInfo.Console;
 internal static class Extensions
 {
-	public static async ValueTask StoreAsync<T>(
-		this FileInfo file,
-		T data)
-	{
-		file.ThrowIfNull();
-		if (data is null) throw new ArgumentNullException(nameof(data));
-
-		using var fs = file.Open(FileMode.Create);
-		await JsonSerializer.SerializeAsync(fs, data);
-	}
-
-	public static async ValueTask<T> RetrieveAsync<T>(
-		this FileInfo file)
-		where T : new()
-	{
-		if (!file.Exists) return new T();
-		using var fs = file.Open(FileMode.Open);
-		return await JsonSerializer.DeserializeAsync<T>(fs) ?? new T();
-	}
-
 	public static T Prompt<T>(
 		this IDictionary<string, string> store,
 		string message,
