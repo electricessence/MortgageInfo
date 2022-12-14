c4e43aa0feee72e445be5209e5e87a8009115ee3
electricessence <5899455+electricessence@users.noreply.github.com>
2022-12-13 23:15:46 -0800

    Added better parsing of numbers and display of taxes saved.

--- MortgageInfo.Console/Extensions.cs
+++ MortgageInfo.Console/Extensions.cs
@@ -21,11 +21,44 @@ internal static class Extensions
 			&& T.TryParse(s, NumberStyles.Any, null, out var v)
 			? v : defaultValue;
 
-		var prompt = new TextPrompt<T>(message).DefaultValue(value);
-		prompt.Converter = converter;
-		var input = AnsiConsole.Prompt(prompt);
-		store[message] = input.ToString().ThrowIfNull();
+		var defaultText = converter is null ? value.ToString().ThrowIfNull() : converter(value);
+		var prompt = new TextPrompt<string>(message)
+			.DefaultValue(defaultText)
+			.Validate(response =>
+			{
+				if (T.TryParse(response, NumberStyles.Any, null, out value))
+					return true;
+
+				var span = response.AsSpan().Trim();
+				if (!T.TryParse(span[..^1], NumberStyles.Any, null, out value))
+					return false;
+
+				dynamic m = span[^1] switch
+				{
+					'B' => 1_000_000_000,
+					'M' => 1_000_000,
+					'K' or 'k' => 1_000,
+					_ => 0
+				};
+
+				if (m is 0) return false;
+
+				value *= m;
+
+				return true;
+			});
+
+		AnsiConsole.Prompt(prompt);
+		store[message] = value.ToString().ThrowIfNull();
 
 		return value;
+	}
+
+	public static TableColumn SetFooter(
+		this TableColumn column,
+		string text)
+	{
+		column.Footer = new Text(text);
+		return column;
 	}
 }
--- MortgageInfo.Console/Program.cs
+++ MortgageInfo.Console/Program.cs
@@ -5,29 +5,24 @@ using MortgageInfo.Console;
 using MortgageInfo.Console.Persistence;
 using Spectre.Console;
 
+var storage = await FileStore<Dictionary<string, string>>.CreateAsync("params.json");
+var data = storage.Data;
+
 AnsiConsole.Write(new Rule("Mortgage Details"));
 ///////////////////////////////////////////////////
-var results = new Mortgage(await GetParameters());
+var results = new Mortgage(GetParameters());
 Summary(results);
+var taxRate = data.Prompt("What is your marginal tax rate?", 25.0M, v => $"{v:0.0}%") / 100;
+await storage.SaveAsync();
 ShowSchedule(results.Schedule);
 ///////////////////////////////////////////////////
 
-static async ValueTask<Parameters> GetParameters()
+Parameters GetParameters() => new()
 {
-	var storage = await FileStore<Dictionary<string, string>>.CreateAsync("params.json");
-	var data = storage.Data;
-
-	var p = new Parameters
-	{
-		LoanAmount = data.Prompt("What is the loan amount?", 500_000.0M, v => v.ToString("C0")),
-		InterestRate = data.Prompt("What is interest rate?", 5.0f) / 100,
-		Years = data.Prompt<ushort>("How many years?", 30)
-	};
-
-	await storage.SaveAsync();
-
-	return p;
-}
+	LoanAmount = data.Prompt("What is the loan amount?", 500_000.0M, v => v.ToString("C0")),
+	InterestRate = data.Prompt("What is interest rate?", 5.0f, v => $"{v:0.000}%") / 100,
+	Years = data.Prompt<ushort>("How many years?", 30)
+};
 
 static void Summary(Mortgage results)
 {
@@ -39,32 +34,61 @@ static void Summary(Mortgage results)
 	AnsiConsole.Write(new Rule());
 }
 
-static void ShowSchedule(IEnumerable<Payment> results)
+decimal TaxSavings(decimal interest) => taxRate * interest;
+
+void ShowSchedule(IEnumerable<Payment> results)
 {
+	AnsiConsole.Write(new Rule());
 	var year = 0;
 	foreach (var payments in results.Batch(12))
 	{
-		if (!AnsiConsole.Confirm($"Show year {++year}?")) break;
-		AnsiConsole.Cursor.MoveUp(1);
+		year++;
+		// Always show the first year.
+		if (year != 1)
+		{
+			var show = AnsiConsole.Confirm($"Show year {year}?");
+			AnsiConsole.Cursor.MoveUp(1);
+			if (!show) break;
+		}
 
 		var table = new Table
 		{
 			Title = new TableTitle($"Year {year:00}")
 		};
-		table.AddColumn("Month");
-		table.AddColumn("Interest");
-		table.AddColumn("Principal");
+
+		var intCol = new TableColumn("Interest").RightAligned();
+		var prinCol = new TableColumn("Principal").RightAligned();
+
+		table
+			.AddColumn(
+				new TableColumn("Month")
+				.SetFooter("Total:")
+				.RightAligned())
+			.AddColumn(intCol)
+			.AddColumn(prinCol);
 
 		var month = 0;
+		var interest = 0M;
+		var principal = 0M;
 		foreach (var payment in payments)
 		{
 			month++;
 			table.AddRow(
-				month.ToString("00"),
+				month.ToString("00 "),
 				payment.Interest.Current.ToString("C2"),
 				payment.Principal.Current.ToString("C2"));
+			interest += payment.Interest.Current;
+			principal += payment.Principal.Current;
 		}
 
+		intCol.SetFooter(interest.ToString("C2"));
+		prinCol.SetFooter(principal.ToString("C2"));
+		table.Caption = new TableTitle($"Tax Savings: {TaxSavings(interest):C2}");
+
 		AnsiConsole.Write(table);
+		AnsiConsole.WriteLine();
 	}
+
+	AnsiConsole.WriteLine();
+	AnsiConsole.WriteLine();
 }
